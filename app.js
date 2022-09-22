6dd0bded3f9f4386ee49aca32eb652483b997c90
Lucas Gallardo <gallardolucasesteban@gmail.com>
2022-09-21 21:29:22 -0300

    segunda entrega

---
+++ app.js
@@ -0,0 +1,89 @@
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
+
+let carrito = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+/* STOCK */
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt= "">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p>Talle: ${producto.talle}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+
+    `
+    contenedorProductos.appendChild(div)
+
+    
+    const boton = document.getElementById(`agregar${producto.id}`)
+    
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id)
+    })
+})
+
+
+//AGREGAR AL CARRITO
+const agregarAlCarrito = (prodId) => {
+
+    // AUMENTAR LA CANTIDAD Y QUE NO SE REPITA
+    const existe = carrito.some (prod => prod.id === prodId) 
+
+    if (existe){ 
+        const prod = carrito.map (prod => { 
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else { 
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        carrito.push(item)
+    }
+    
+    actualizarCarrito() 
+}
+
+
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach ( (prod) => {
+        const div = document.createElement ('div')
+        div.className = ('productoEnCarrito') 
+        div.innerHTML = ` 
+            <p>${prod.nombre}</p>
+            <p>Precio:$ ${prod.precio} </p>
+            <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+            
+            </button>`;
+        contenedorCarrito.appendChild (div)
+        
+
+            localStorage.setItem ('carrito', JSON.stringify(carrito))  
+} ,
+        contadorCarrito.innerText = carrito.length,
+        precioTotal.innerText = carrito.reduce ((acc, producto) => acc + producto.precio*producto.cantidad, 0)
+)}
---
+++ stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, nombre: "Buzo 1", tipo: "buzo", cantidad: 1, desc: "Buzo para el invierno", precio: 5000, talle: "L", img: './img/buzo.jpg'},
+    {id: 2, nombre: "Remera 1", tipo: "remera", cantidad: 1, desc: "Remera para salir de fiesta", precio: 3500, talle: "L", img: './img/images(1).jpg'},
+    {id: 3, nombre: "Remera 2", tipo: "remera", cantidad: 1, desc: "Remera para salir de fiesta", precio: 3500, talle: "L", img: './img/images(2).jpg'},
+    {id: 4, nombre: "Remera 3", tipo: "remera", cantidad: 1, desc: "Remera para salir de fiesta", precio: 3500, talle: "M", img: './img/images(3).jpg'},
+    {id: 5, nombre: "Zapatillas 1", tipo: "zapatillas", cantidad: 1, desc: "Zapatillas diarias", precio: 700, talle: "M", img: './img/zapatillas.jpg'},
+  
+]
